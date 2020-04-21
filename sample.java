d7221ff94ba6058d92dd8241530e33a6c8d0e759
priyanka1012-singhal <priyanka.singhal@sjsu.edu>
2020-04-20 17:31:54 -0700

    Add Clone Graph

--- sample.java
+++ sample.java
@@ -1,7 +1,26 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
-
-
-// Your code here along with comments explaining your approach
+// DFS
+// Time - O(n) or O(v * E)
+// Space - O(n) for queue and HashMap
+class Solution {
+    HashMap<Node, Node> map;
+    public Node cloneGraph(Node node) {
+        if(node == null)
+            return null;
+        map = new HashMap<>();
+        dfs(node);
+        
+        return map.get(node);
+    }
+    
+    private void dfs(Node node){
+         if(node == null)
+            return;
+        map.put(node, new Node(node.val));
+        for(Node n: node.neighbors){
+            if(!map.containsKey(n))
+                dfs(n);
+            map.get(node).neighbors.add(map.get(n));
+        }
+        
+    }
+}
