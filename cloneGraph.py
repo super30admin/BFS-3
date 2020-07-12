bb05ecc86b0172af69bcc922a103458b0e982c3b
Sonali B Sreedhar <50754966+sbsreedh@users.noreply.github.com>
2020-07-11 17:45:29 -0700

    Create cloneGraph.py

---
+++ cloneGraph.py
@@ -0,0 +1,28 @@
+#Time Complexity:O(v+e)
+#Space COmplexity:O(v)
+#Ran successfully on Leetcode:Yes
+#Algorithm:
+#Create a hashmap to store the nodes and its copies.
+#Create a copyNode of the nodes traversed and add to hashMap wrt to its node if it doesnt exist already.
+# Always add the neighbor links while graph traversal.
+#return the copyNode
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        q=[]
+        q.append(node)
+        seen={}
+        copyNode=Node(node.val)
+        seen[node]=copyNode
+        while q:
+            curr=q.pop()
+            for n in curr.neighbors:
+                if n not in seen:
+                    seen[n]=Node(n.val)
+                    q.append(n)
+                seen[curr].neighbors.append(seen[n])
+        return copyNode
+        
+        
+        
