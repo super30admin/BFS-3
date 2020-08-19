29efb0de7f97b11555a656d5db4bd335ad95c6a5
Damanpreet Kaur <kaurd@oregonstate.edu>
2020-08-19 11:49:35 -0700

    Update clone graph

--- clonegraph.py
+++ clonegraph.py
@@ -32,3 +32,27 @@ class Solution:
                 
         return copy_root
         
+
+# Recursive solution
+# Space complexity - O(2V) -- for hash map and recursive stack (worst case recursive stack - O(n))
+# Time complexity - O(V+E) 
+class Solution:
+    def __init__(self):
+        self.hashNodes = dict()
+        
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node: return 
+        
+        if node.val in self.hashNodes:
+            return self.hashNodes[node.val]
+        
+        # keep a copy of the node
+        clone_node = Node(node.val)
+        self.hashNodes[node.val] = clone_node
+        
+        # assign neighbors to the cloned node.
+        clone_node.neighbors = [self.cloneGraph(neigh) for neigh in node.neighbors]
+            
+        return clone_node
+    
+    
