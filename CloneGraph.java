65ecc2db26164517df695a12a0ceb359bed24b67
unknown <harale.s@husky.neu.edu>
2020-08-19 21:09:27 -0400

    Completed BFS-3

---
+++ CloneGraph.java
@@ -0,0 +1,57 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null)
+            return null;
+        Queue<Node> q = new LinkedList<>();
+
+        HashMap<Integer,Node> map = new HashMap<>();
+        q.add(node);
+        Node newNode = new Node(node.val,new ArrayList<>());
+        map.put(newNode.val,newNode);
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+
+            for(Node neighbor : curr.neighbors){
+
+                if(!map.containsKey(neighbor.val)){
+                    q.add(neighbor);
+                    Node nnode = new Node(neighbor.val,new ArrayList<>());
+                    map.put(neighbor.val,nnode);
+                }
+
+                map.get(curr.val).neighbors.add(map.get(neighbor.val));
+
+            }  
+        }
+
+        return newNode;
+    }
+} 
