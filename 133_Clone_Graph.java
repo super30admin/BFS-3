6d69b3634bed6ad9bbe86b8e7c1589caa994fe16
laxmikantbpandhare <laxmikantpandhare@gmail.com>
2020-11-04 16:59:13 -0800

    added problems

--- 133_Clone_Graph.java
+++ 133_Clone_Graph.java
@@ -14,4 +14,57 @@
 
     */  
 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null){
+            return node;
+        }
+        
+        hm = new HashMap<>();
+        
+        return dfs(node);
+    }
+    
+    HashMap<Integer, Node> hm;
+    
+    private Node dfs(Node node){
+        if(hm.containsKey(node.val)){
+            return hm.get(node.val);
+        }
+        
+        Node newNode = new Node(node.val, new ArrayList<>());
+        
+        hm.put(node.val, newNode);
+        
+        for(Node n : node.neighbors){
+            hm.get(node.val).neighbors.add(dfs(n));
+        }
+        
+        return newNode;
+    }
+}
+
 
