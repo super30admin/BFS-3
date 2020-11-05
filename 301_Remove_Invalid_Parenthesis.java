5698a5c2d40e72edf266655ea170be61aac88bb0
laxmikantbpandhare <laxmikantpandhare@gmail.com>
2020-11-04 16:41:48 -0800

    added problems

--- 301_Remove_Invalid_Parenthesis.java
+++ 301_Remove_Invalid_Parenthesis.java
@@ -1,7 +1,7 @@
     /*  Explanation
     # Leetcode problem link : https://leetcode.com/problems/remove-invalid-parentheses/
-    Time Complexity for operators : o(n^n) .. n is the length of the string
-    Extra Space Complexity for operators : o(n) for (List<String> path) without recursive stack
+    Time Complexity for operators : o(2^n) .. n is the length of the string
+    Extra Space Complexity for operators : o(n) for (queue and hashset)
     Did this code successfully run on Leetcode : NA
     Any problem you faced while coding this : No
 # Your code here along with comments explaining your approach
