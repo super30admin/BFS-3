21f5786e736ecf0890a4575a9c85c90e12c95719
Dhruv <jawalkad@oregonstate.edu>
2021-02-07 18:35:35 -0800

    bfs-3

---
+++ Remove Invalid Parentheses.java
@@ -0,0 +1,67 @@
+// Time Complexity : O(n*2^n)
+// Space Complexity : O(C(n,k) + n), this is the max size of queue, (n-k)*nCk
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+// Your code here along with comments explaining your approach
+//approach is to do a bfs approach, at first level dont remove any char, check if valid, if yes append to res and return res guarentees min chars removed
+//else generate all possiblities to remove 1 char and verify if valid, if we find first answer, just check answers at that level and return result
+
+public class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+      List<String> res = new ArrayList<>();
+      
+      // sanity check
+      if (s == null) return res;
+      
+      Set<String> visited = new HashSet<>();
+      Queue<String> queue = new LinkedList<>();
+      
+      // initialize
+      queue.add(s);
+      visited.add(s);
+      
+      boolean found = false;
+      
+      while (!queue.isEmpty()) {
+        s = queue.poll();
+        
+        if (isValid(s)) {
+          // found an answer, add to the result
+          res.add(s);
+          found = true;
+        }
+      
+        if (found) continue;
+      
+        // generate all possible states
+        for (int i = 0; i < s.length(); i++) {
+          // we only try to remove left or right paren
+          if (s.charAt(i) != '(' && s.charAt(i) != ')') continue;
+        
+          String t = s.substring(0, i) + s.substring(i + 1);
+        
+          if (!visited.contains(t)) {
+            // for each state, if it's not visited, add it to the queue
+            queue.add(t);
+            visited.add(t);
+          }
+        }
+      }
+      
+      return res;
+    }
+    
+    // helper function checks if string s contains valid parantheses
+    boolean isValid(String s) {
+      int count = 0;
+    
+      for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+        if (c == '(') count++;
+        if (c == ')' && count-- == 0) return false;
+      }
+    
+      return count == 0;
+    }
+}
