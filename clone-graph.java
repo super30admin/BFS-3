520239cbc305c82b085eeffdfeab0ebec922c3ce
Rishi Raj <rishiraj824@gmail.com>
2021-02-20 18:49:51 -0500

    Update clone-graph.java

--- clone-graph.java
+++ clone-graph.java
@@ -1,3 +1,50 @@
+// DFS - O(N), O(N)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    HashMap<Node, Node> map;       
+    
+    public Node cloneGraph(Node node) {
+        if(node == null) {
+            return node;
+        }
+        map = new HashMap<>(); 
+        
+        dfs(node);
+        return map.get(node);
+        
+    }
+    private void dfs(Node node) {
+        if(map.containsKey(node)) {
+            return;
+        }
+        Node copyNode = new Node(node.val);
+        map.put(node, copyNode);
+        for(Node n : node.neighbors) {
+            dfs(n);
+            map.get(node).neighbors.add(map.get(n));
+        }
+    }
+}
+// BFS
 //Time,Space-O(N),O(N)
 /*
 // Definition for a Node.
