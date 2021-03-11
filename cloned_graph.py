ff0cc41d127def098240d7900bda09aa2c59643f
Snehapiriya Tlk <snehasg95@gmail.com>
2021-03-10 17:21:01 -0800

    Added BFS Approach

--- cloned_graph.py
+++ cloned_graph.py
@@ -47,6 +47,53 @@ class Solution:
             
                                          
     
+
+## BFS Approach
+# Time - O( V + E)
+# Space - O(V + E)
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        
+        if not node:
+            return node
+        
+        hashmap = {}
+        
+        queue = collections.deque()
+        
+        cloned = Node(node.val)
+        hashmap[node] = cloned
+        
+        queue.append(node)
+        
+        while queue:
+            
+            current = queue.popleft()
+            
+            for neighbor in current.neighbors:
+                
+                if neighbor not in hashmap:
+                    copy = Node(neighbor.val)
+                    
+                    hashmap[neighbor] = copy
+                    queue.append(neighbor)
+                    
+                hashmap[current].neighbors.append(hashmap[neighbor])
+                
+        return cloned
+            
+            
+        
+        
         
     
     
