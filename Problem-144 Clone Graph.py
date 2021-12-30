55c55fc338e0172e441994e17878e2459a2aa5dc
Adhish Shrivastava <adhish70@gmail.com>
2021-12-30 10:21:05 -0500

    Problem 144 done

--- Problem-144 Clone Graph.py
+++ Problem-144 Clone Graph.py
@@ -1,10 +1,14 @@
 # 133. Clone Graph
 # https://leetcode.com/problems/clone-graph/
 
-# Logic: 
+# Logic: We make a hashmap with original node as key and a copy node as value. 
+# We will use the hashmap as a visited set also. Start from node given and 
+# create its children if copy is not created. Only visit nodes who's copy 
+# has not been cretaed. When the copy is created only then we add it to 
+# queue as it has not been visted so far.
 
-# Time Complexity:
-# Space Complexity:
+# Time Complexity: O(V+E)
+# Space Complexity: O(V)
 
 """
 # Definition for a Node.
@@ -13,6 +17,26 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
+from collections import deque
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        pass
+        if node is None:
+            return None
+        
+        hashmap = dict()
+        hashmap[node] = Node(node.val)
+        
+        q = deque()
+        q.append(node)
+        
+        while q:
+            curNode = q.popleft()
+            neigh = curNode.neighbors
+            
+            for i in neigh:
+                if i not in hashmap:
+                    hashmap[i] = Node(i.val)
+                    q.append(i)
+                hashmap[curNode].neighbors.append(hashmap[i])
+        
+        return hashmap[node]
