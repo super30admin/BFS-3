1a9909d809b2cedc103a36db3a5fdc16728d4dda
LiberoPat <patrick.trias@gmail.com>
2022-03-18 13:28:39 -0700

    complete

--- p1RemoveInvalidPRarn.py
+++ p1RemoveInvalidPRarn.py
@@ -2,16 +2,35 @@
 time: O(N)
 space: O(N)
 """
-
-def removeInvalidParentheses(self, s):
-    def isvalid(s):
-        s = filter('()'.count, s)
-        while '()' in s:
-            s = s.replace('()', '')
-        return not s
-    level = {s}
-    while True:
-        valid = filter(isvalid, level)
-        if valid:
-            return valid
-        level = {s[:i] + s[i+1:] for s in level for i in range(len(s))}
+import collections
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        q = collections.deque([s])
+        res = []
+        visited = set()
+        visited.add(s)
+        found = False
+        while q:
+            strr = q.popleft()
+            if self.isValid(strr):
+                res.append(strr)
+                found = True
+            if found: continue
+            for i in range(len(strr)):
+                if strr[i]=='(' or strr[i]==')':
+                    ss = strr[:i] + strr[i+1:]
+                    if ss not in visited:
+                        q.append(ss)
+                        visited.add(ss)
+        return res
+       
+    def isValid(self, s):
+        count = 0
+        for c in s:
+            if c == '(':
+                count+=1
+            elif c == ')':
+                count-=1
+            if count<0:
+                return False
+        return count == 0
