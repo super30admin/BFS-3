d0c764f133726b6d82b3b85dab4dc05fa1f0cb2f
skatkar <skatkar225@gmail.com>
2022-06-14 18:40:37 -0500

    BFS-3 submission

---
+++ CloneGraph.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+public class CloneGraph {
+    // BFS Approach
+    // TC : O(V + E)
+    // SC : O(V)
+    Map<Node, Node> map;
+    public Node cloneGraph_BFS(Node node) {
+        if(node == null) return null;
+
+        map = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+        clone(node);
+        q.add(node);
+
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+            List<Node> neighbors = curr.neighbors;
+            for(Node n : neighbors) {
+                if(!map.containsKey(n)) { // Kind of a marking a node as visited. Else it will clone the node which is already cloned
+                    clone(n);
+                    q.add(n);
+                }
+                map.get(curr).neighbors.add(map.get(n));
+            }
+        }
+
+        return map.get(node);
+    }
+
+    private Node clone(Node node) {
+        Node copyNode = new Node(node.val);
+        map.put(node, copyNode);
+        return copyNode;
+    }
+}
