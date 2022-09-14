c51ac8e233c728d21dd2435971d2ff90290e16da
Shubham <shub01gupta@gmail.com>
2022-09-14 11:25:19 +0530

    Completed BFS-3

---
+++ removeinvalidparenthesis.java
@@ -0,0 +1,56 @@
+//TC : Exponential
+//SC : Exponential
+
+class Solution {
+    HashSet<String> set;
+    List<String> result;
+
+    public List<String> removeInvalidParentheses(String s) {
+        if (s == null || s.length() == 0)
+            return new ArrayList<>();
+
+        set = new HashSet<>();
+        result = new ArrayList<>();
+        Queue<String> q = new LinkedList<>();
+        boolean found = false;
+        q.add(s);
+        while (!q.isEmpty() && !found) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                String curr = q.poll();
+                if (isValid(curr)) {
+                    found = true;
+                    result.add(curr);
+                } else if (found == false) {
+                    for (int j = 0; j < curr.length(); j++) {
+                        if (curr.charAt(j) >= 'a' && curr.charAt(j) <= 'z') {
+                            continue;
+                        }
+                        String baby = curr.substring(0, j) + curr.substring(j + 1);
+                        if (!set.contains(baby)) {
+                            set.add(baby);
+                            q.add(baby);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+
+    }
+
+    private boolean isValid(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+                if (count < 0)
+                    return false;
+            }
+        }
+        return count == 0;
+    }
+}
