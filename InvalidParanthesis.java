ab5fa0b51e72f6677c014eb31d553177498610a4
Shivani31996 <shivani.chavan96@gmail.com>
2022-12-23 15:20:24 -0500

    Please review

--- InvalidParanthesis.java
+++ InvalidParanthesis.java
@@ -90,3 +90,78 @@ public class InvalidParanthesis {
         return count == 0;
     }
 }
+
+
+/*
+ * Approach 2 - DFS
+ * Optimization - If any incoming string's length is less than the length of the string added to the result,
+ * then we will not perform DFS on its babies.
+ */
+class Solution {
+    List<String> result;
+    HashSet<String> set;
+    int max;
+    public List<String> removeInvalidParentheses(String s) {
+        result = new ArrayList<>();
+        set = new HashSet<>();
+        
+        if(s == null)
+            return result;
+        
+        dfs(s);
+        return result;
+    }
+    
+    private void dfs(String s)
+    {
+        //base
+        if(set.contains(s))
+            return;
+        if(max > s.length())
+            return;
+        
+        //logic
+        set.add(s);
+        if(isValid(s))
+        {
+            if(max != s.length())
+            {
+                result = new ArrayList<>();
+            }
+            max = s.length();
+            result.add(s);
+        }
+        else
+        {
+            for(int i=0; i<s.length(); i++)
+            {
+                char ch = s.charAt(i);
+                if(Character.isLetter(ch))
+                    continue;
+                String child = s.substring(0,i) + s.substring(i+1);
+                dfs(child);
+            }
+        }
+    }
+    
+    private boolean isValid(String s)
+    {
+        int count = 0;
+        for(int i=0; i<s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if(Character.isLetter(ch))
+                continue;
+            else if(ch == '(')
+                count++;
+            else if(ch == ')')
+            {
+                if(count == 0)
+                    return false;
+                else
+                    count--;
+            }
+        }
+        return count == 0;
+    }
+}
