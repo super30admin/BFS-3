83439ce10a3b241aabab46a806b11ba0d3ac8da9
rutujamandlikFT <123407038+rutujamandlikFT@users.noreply.github.com>
2023-06-16 18:23:25 -0400

    Create clonegraphDFS.java

---
+++ clonegraphDFS.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+       
+        HashMap<Node, Node> map ;
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+        map = new HashMap<>();
+        dfs(node);
+        return map.get(node);
+    }
+    
+    private void dfs(Node node){
+        //base
+        if (map.containsKey(node)) return;
+        
+        //logic
+        Node newNode = new Node(node.val);
+        map.put(node, newNode);
+        for(Node n: node.neighbors){
+            if(!map.containsKey(n)){
+                dfs(n);
+            }
+            map.get(node).neighbors.add(map.get(n));
+        }
+        
+    }
+}
