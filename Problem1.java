bbc4c0d4ebf9d5f7c6b169a1a9a56ca010776ab0
Shreya Agrawal <84276060+ag-sh1996@users.noreply.github.com>
2024-03-13 00:38:41 -0500

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,12 +1,37 @@
 class Solution {
     //TC: O(n^2)
     //SC: O(n)
-    HashSet<String> set;
-    List<String> result;
     public List<String> removeInvalidParentheses(String s) {
-       this.set = new HashSet<>();
-       this.result = new ArrayList<>();
-        
+        HashSet<String> set = new HashSet<>();
+        List<String> result = new ArrayList<>();
+        Queue<String> q = new LinkedList<>();
+        set.add(s);
+        q.add(s);
+        boolean flag = false;
+        while(!q.isEmpty() && !flag){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                String curr = q.poll();
+                if(isValid(curr)){ 
+                    flag = true;
+                    result.add(curr);
+                }
+                if(!flag){
+                //remove 1 char at a time
+                    for(int j = 0; j < curr.length(); j++){
+                        char c = curr.charAt(j);
+                        if(!Character.isAlphabetic(c)){
+                            String child = curr.substring(0,j) + curr.substring(j+1);
+                            if(!set.contains(child)){
+                                set.add(child);
+                                q.add(child);
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+        return result;
     }
 
     private boolean isValid(String s){
